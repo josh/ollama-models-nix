552e70ed2a855f59a306551ef18f6654929ede1d
Joshua Peek <josh@users.noreply.github.com>
2024-12-09 18:24:46 -0800

    Just use dirs as index

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
         "x86_64-darwin"
         "x86_64-linux"
       ];
-      models = builtins.fromJSON (builtins.readFile ./manifests/models.json);
+      models = builtins.attrNames (builtins.readDir ./manifests/registry.ollama.ai/library);
     in
     {
       packages = lib.genAttrs systems (
@@ -23,17 +23,21 @@
         let
           pkgs = nixpkgs.legacyPackages.${system};
           callPackage = pkgs.callPackage;
+          modelTags =
+            model: builtins.attrNames (builtins.readDir ./manifests/registry.ollama.ai/library/${model});
           mkModel = model: tag: callPackage ./model.nix { inherit model tag; };
           mkModelCollection =
-            model: tags:
+            model:
             (mkModel model "latest")
-            // (lib.genAttrs tags (mkModel model))
+            // (lib.genAttrs (modelTags model) (mkModel model))
             // ({ recurseForDerivations = true; });
         in
-        (lib.attrsets.mapAttrs' (model: tags: {
-          name = lib.strings.replaceStrings [ "." ] [ "_" ] model;
-          value = mkModelCollection model tags;
-        }) models)
+        (builtins.listToAttrs (
+          builtins.map (model: {
+            name = lib.strings.replaceStrings [ "." ] [ "_" ] model;
+            value = mkModelCollection model;
+          }) models
+        ))
         // {
           update-manifests = callPackage ./update.nix { };
         }
