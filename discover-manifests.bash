f6396d637eff97c6ed3cfb8a65bf1b5a337e62aa
Joshua Peek <josh@users.noreply.github.com>
2024-12-10 14:51:29 -0800

    Discover tags

--- discover-manifests.bash
+++ discover-manifests.bash
@@ -4,15 +4,23 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+HTML="$(curl https://ollama.com/search)"
+
 models() {
-	curl https://ollama.com/search |
+	echo "$HTML" |
 		htmlq 'a[href^="/library/"]' --attribute href |
 		sed 's/^\/library\///'
 }
 
+tags() {
+	echo "latest"
+	echo "$HTML" |
+		htmlq "a[href=\"/library/$1\"] > div:nth-child(2) > div:nth-child(1) > span" --text
+}
+
 for model in $(models); do
 	mkdir -p "manifests/registry.ollama.ai/library/$model"
-	touch "manifests/registry.ollama.ai/library/$model/latest"
+	for tag in $(tags "$model"); do
+		touch "manifests/registry.ollama.ai/library/$model/$tag"
+	done
 done
-
-# TODO: Discover tags for each model
