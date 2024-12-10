1e630b82e7a93c45eb191049fc240cb6d3e69a34
Joshua Peek <josh@users.noreply.github.com>
2024-12-10 14:02:37 -0800

    Support updating individual model

--- discover-manifests.bash
+++ discover-manifests.bash
@@ -5,14 +5,14 @@ set -o nounset
 set -o pipefail
 
 models() {
-  curl https://ollama.com/search | \
-    htmlq 'a[href^="/library/"]' --attribute href | \
-    sed 's/^\/library\///'
+	curl https://ollama.com/search |
+		htmlq 'a[href^="/library/"]' --attribute href |
+		sed 's/^\/library\///'
 }
 
 for model in $(models); do
-  mkdir -p "manifests/registry.ollama.ai/library/$model"
-  touch "manifests/registry.ollama.ai/library/$model/latest"
+	mkdir -p "manifests/registry.ollama.ai/library/$model"
+	touch "manifests/registry.ollama.ai/library/$model/latest"
 done
 
 # TODO: Discover tags for each model
--- update-manifests.bash
+++ update-manifests.bash
@@ -5,24 +5,35 @@ set -o nounset
 set -o pipefail
 
 pull() {
-  local model=$1
-  local tag=$2
-  local output=$3
-  echo "+ pull $model:$tag"
-  curl --fail \
-    --location \
-    --show-error \
-    --silent \
-    --header "Accept: application/vnd.docker.distribution.manifest.v2+json" \
-    "https://registry.ollama.ai/v2/library/$model/manifests/$tag" \
-    --create-dirs \
-    --output "$output"
+	local model=$1
+	local tag=$2
+	local output=$3
+	echo "+ pull $model:$tag"
+	curl --fail \
+		--location \
+		--show-error \
+		--silent \
+		--header "Accept: application/vnd.docker.distribution.manifest.v2+json" \
+		"https://registry.ollama.ai/v2/library/$model/manifests/$tag" \
+		--create-dirs \
+		--output "$output"
 }
 
-for model_path in "./manifests/registry.ollama.ai/library/"*; do
-  model=$(basename "$model_path")
-  for tag_path in "$model_path"/*; do
-    tag=$(basename "$tag_path")
-    pull "$model" "$tag" "$tag_path"
-  done
-done
+pull_all() {
+	for model_path in "./manifests/registry.ollama.ai/library/"*; do
+		model=$(basename "$model_path")
+		for tag_path in "$model_path"/*; do
+			tag=$(basename "$tag_path")
+			pull "$model" "$tag" "$tag_path"
+		done
+	done
+}
+
+MODEL="${1:-}"
+TAG="${2:-latest}"
+
+if [ -n "$MODEL" ]; then
+	pull "$MODEL" "$TAG" "./manifests/registry.ollama.ai/library/$MODEL/$TAG"
+else
+	pull_all
+fi
