3675066c01bdd0bae0fee78814aa4a0e0dae132a
Joshua Peek <josh@users.noreply.github.com>
2024-12-11 23:46:44 -0800

    Use mkDerivation directly

--- ollama-model.nix
+++ ollama-model.nix
@@ -1,28 +1,24 @@
 {
   lib,
   fetchurl,
-  runCommand,
+  stdenvNoCC,
   registry ? "registry.ollama.ai",
   modelNamespace ? "library",
   model,
   tag ? "latest",
 }:
 let
-  validateModelTag =
-    { model, tag }:
-    let
-      parts = lib.strings.splitString ":" model;
-      model' = builtins.elemAt parts 0;
-      tag' = builtins.elemAt parts 1;
-      modelHasTag = builtins.length parts == 2;
-    in
+  modelParts = lib.strings.splitString ":" model;
+  modelHasTag = builtins.length modelParts == 2;
+
+  meta =
     if modelHasTag then
       assert lib.asserts.assertMsg (
         tag == "latest"
       ) "'${model}' already has a tag, but '${tag}' was given";
       {
-        model = model';
-        tag = tag';
+        model = builtins.elemAt modelParts 0;
+        tag = builtins.elemAt modelParts 1;
       }
     else
       {
@@ -30,16 +26,15 @@ let
         tag = tag;
       };
 
-  id = validateModelTag { inherit model tag; };
-
-  manifestPath = ./manifests/${registry}/${modelNamespace}/${id.model}/${id.tag}.json;
+  qualifiedModel = "${modelNamespace}/${meta.model}";
+  manifestPath = ./manifests/${registry}/${qualifiedModel}/${meta.tag}.json;
   manifest = builtins.fromJSON (builtins.readFile manifestPath);
 
   fetchblob =
     sha256:
     fetchurl {
       name = "sha256-${sha256}";
-      url = "https://${registry}/v2/${modelNamespace}/${id.model}/blobs/sha256:${sha256}";
+      url = "https://${registry}/v2/${qualifiedModel}/blobs/sha256:${sha256}";
       inherit sha256;
     };
 
@@ -57,17 +52,18 @@ let
   blobs = linkblobs ([ manifest.config ] ++ manifest.layers);
 in
 
-runCommand "ollama-model-${id.model}-${id.tag}"
-  {
-    meta = {
-      model = id.model;
-      tag = id.tag;
-      homepage = "https://ollama.com/library/${id.model}:${id.tag}";
-      platforms = lib.platforms.all;
-    };
-  }
-  ''
-    mkdir -p $out/manifests/${registry}/${modelNamespace}/${id.model} $out/blobs
-    cp ${manifestPath} $out/manifests/${registry}/${modelNamespace}/${id.model}/${id.tag}
+stdenvNoCC.mkDerivation {
+  name = "ollama-model-${meta.model}-${meta.tag}";
+
+  buildCommand = ''
+    mkdir -p $out/manifests/${registry}/${qualifiedModel} $out/blobs
+    cp ${manifestPath} $out/manifests/${registry}/${qualifiedModel}/${meta.tag}
     ${builtins.concatStringsSep "\n" blobs}
-  ''
+  '';
+  passAsFile = [ "buildCommand" ];
+
+  meta = meta // {
+    homepage = "https://ollama.com/library/${meta.model}:${meta.tag}";
+    platforms = lib.platforms.all;
+  };
+}
